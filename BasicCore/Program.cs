02b965e858542fab6b25640a0724e97c07975d86
divatepratik10 <pratikrdivate3210@gmail.com>
2022-04-01 11:38:15 +0530

    [Pratik] added:Welcome message

---
+++ BasicCore/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Programms.");
+            Console.ReadLine();
+        }
+    }
+}
